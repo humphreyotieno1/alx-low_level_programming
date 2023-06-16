bbfa8f37986ada4808dc1eaee82b68b51c2dea92
Humphrey <humphreyotieno04@gmail.com>
2023-06-16 15:05:42 -0700

    Variables, If, Else, While

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,15 +12,15 @@ int main(void)
 	for (n = 48; n <= 56; n++)
 	{
 		for (m = 49; m <= 57; m++)
-		{	
+		{
 			if (m > n)
 			{
-			putchar(n);
-			putchar(m);
+				putchar(n);
+				putchar(m);
 				if (n != 56 || m != 57)
 				{
-				putchar(',');
-				putchar(' ');
+					putchar(',');
+					putchar(' ');
 				}
 			}
 		}
