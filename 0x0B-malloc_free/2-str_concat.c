f0426fedcfe3cc6d909be5f76ce088376607b619
Humphrey <humphreyotieno04@gmail.com>
2023-07-11 13:53:44 -0700

    Malloc and free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,37 +10,36 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	char *conct;
+	int i_s1, i_s2, len1, len2;
+	char *a;
 
-	int i, ci;
-
-	if (s1 == NULL)
+	if (s1 == 0)
+	{
 		s1 = "";
-	if (s2 == NULL)
+	}
+	for (len1 = 0; s1[len1] != 0; len1++)
+	{
+	}
+	if (s2 == 0)
+	{
 		s2 = "";
-
-	i = ci = 0;
-
-	while (s1[i] != '\0')
-		i++;
-	while (s2[i] != '\0')
-		ci++;
-	conct = malloc(sizeof(char) * (i + ci + 1));
-
-	if (conct == NULL)
-		return (NULL);
-	i = ci = 0;
-	while (s1[i] != '\0')
-	{
-		conct[i] = s1[i];
-		i++;
 	}
-
-	while (s2[ci] != '\0')
+	for (len2 = 0; s2[len2] != 0; len2++)
 	{
-		conct[i] = s2[ci];
-		i++, ci++;
 	}
-	conct[i] = '\0';
-	return (conct);
+	a = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (a == 0)
+	{
+		return (0);
+	}
+	for (i_s1 = 0; i_s1 < len1; i_s1++)
+	{
+		a[i_s1] = s1[i_s1];
+	}
+	for (i_s2 = 0; i_s2 < len2; i_s1++, i_s2++)
+	{
+		a[i_s1] = s2[i_s2];
+	}
+	a[i_s1] = 0;
+	return (a);
 }
