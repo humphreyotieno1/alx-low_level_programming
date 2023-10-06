62c33c7bb64f15bf5f6b4f25d6fe23c8c87ac39e
humphreyotieno1 <humphreyotieno04@gmail.com>
2023-10-06 14:21:43 -0700

    Retrieve value

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -15,7 +15,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	if (!ht || !key)
 		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
-	tmp = (ht->array)[index];
+	tmp = (ht->array[index]);
 
 	while (tmp != NULL && strcmp(tmp->key, key) != 0)
 		tmp = tmp->next;
