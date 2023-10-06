12ce3dd731ac2ba00df2bef8b92d8623fb00f530
humphreyotieno1 <humphreyotieno04@gmail.com>
2023-10-06 14:47:47 -0700

    Value associated

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,18 +9,18 @@
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int idx;
+	unsigned long int index;
 	hash_node_t *tmp;
 
 	if (!ht || !key)
 		return (NULL);
-	idx = key_index((const unsigned char *)key, ht->size);
-	tmp = (ht->array[idx]);
+	index = key_index((const unsigned char *)key, ht->size);
+	tmp = (ht->array[index]);
 
 	while (tmp != NULL && strcmp(tmp->key, key) != 0)
 		tmp = tmp->next;
 	if (!tmp)
 		return (NULL);
 	else
-		return(tmp->value);
+		return (tmp->value);
 }
