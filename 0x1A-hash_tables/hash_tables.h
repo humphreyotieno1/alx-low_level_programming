8a018c5bdb03ab2749f5fe47fb491b151c0065cb
humphreyotieno1 <humphreyotieno04@gmail.com>
2023-10-06 15:14:48 -0700

    Print hash table

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -39,6 +39,8 @@ unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
+
 /**
  * struct shash_node_s - Node of a sorted hash table
  * @key: The key, string
@@ -77,5 +79,6 @@ typedef struct shash_table_s
 shash_table_t *shash_table_create(unsigned long int size);
 int shash_table_set(shash_table_t *ht, const char *key, const char *value);
 char *shash_table_get(const shash_table_t *ht, const char *key);
+void shash_table_print(const shash_table_t *ht);
 
 #endif
