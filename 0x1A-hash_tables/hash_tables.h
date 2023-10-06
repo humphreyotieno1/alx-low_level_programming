5f31360defc62251e39a35928b12ea465bb5e860
humphreyotieno1 <humphreyotieno04@gmail.com>
2023-10-06 14:45:11 -0700

    Add value

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -37,6 +37,7 @@ typedef struct hash_table_s
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 /**
  * struct shash_node_s - Node of a sorted hash table
@@ -74,6 +75,7 @@ typedef struct shash_table_s
 } shash_table_t;
 
 shash_table_t *shash_table_create(unsigned long int size);
+int shash_table_set(shash_table_t *ht, const char *key, const char *value);
 char *shash_table_get(const shash_table_t *ht, const char *key);
 
 #endif
