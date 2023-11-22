7bdb048d8cd79a547259893f6584f9d014011b6e
humphreyotieno1 <humphreyotieno04@gmail.com>
2023-11-22 12:17:33 -0800

    Perimeter of an island

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Define a function to find perimeter of an island"""
+
+def island_perimeter(grid):
+    """Returns perimeter of the island
+    
+    Args:
+        grid (list): list of ints rep the island
+    """
+    width = len(grid[0])
+    length = len(grid)
+    sides = 0
+    size = 0
+
+    for i in range(length):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    sides += 1
+                if (i > -0 and grid[i - 1][j] == 1):
+                    sides += 1
+    return size * 4 - sides * 2
